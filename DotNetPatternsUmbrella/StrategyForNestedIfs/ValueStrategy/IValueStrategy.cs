d54fa142c8e12abcb47277a7caa73e53d5a66690
jhoyuen <jonathan.klcflam@gmail.com>
2025-02-14 22:37:55 +1100

    JDP-2: use strategy pattern with if statements

---
+++ DotNetPatternsUmbrella/StrategyForNestedIfs/ValueStrategy/IValueStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyForNestedIfs
+{
+    internal interface IValueStrategy
+    {
+        void Evaluate(int value);
+    }
+}
